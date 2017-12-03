0cc3a6676bc417851b7e9e1ed0f1142904248638
santrasatrajit <santrasatrajit@gmail.com>
2017-12-03 18:54:20 -0500

    implemented custom scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+
+3.times do |topic|
+  Topic.create!(
+    title:"Topic #{topic}"
+    )
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
@@ -12,7 +21,8 @@
      into electronic typesetting, remaining essentially unchanged. It was popularised 
      in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more 
      recently with desktop publishing software like Aldus PageMaker including
-      versions of Lorem Ipsum."
+      versions of Lorem Ipsum.",
+    topic_id:Topic.last.id
   )
 end
 
@@ -27,10 +37,31 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio|
+8.times do |portfolio|
   Portfolio.create!(
     title:"Portfolio title: #{portfolio}",
-    subtitle:"My great service",
+    subtitle:"Ruby on Rails",
+    body:"Lorem ipsum dolor sit amet, 
+    consectetur adipiscing elit, sed do 
+      usmod tempor incididunt ut labore et 
+      olore magna aliqua. Ut enim ad
+       minim veniam, quis nostrud exercitation 
+       ullamco laboris nisi ut aliquip ex ea commodo 
+       consequat. Duis aute irure dolor in 
+       reprehenderit in voluptate velit esse 
+       cillum dolore eu fugiat nulla pariatur. 
+       cepteur sint occaecat cupidatat 
+       non proident, sunt in culpa qui officia 
+       deserunt mollit anim id est laborum.",
+    main_img:"http://placehold.it/600x400",
+    thumb_img:"http://placehold.it/350x200"
+  )
+end
+
+1.times do |portfolio|
+  Portfolio.create!(
+    title:"Portfolio title: #{portfolio}",
+    subtitle:"Angular",
     body:"Lorem ipsum dolor sit amet, 
     consectetur adipiscing elit, sed do 
       usmod tempor incididunt ut labore et 
