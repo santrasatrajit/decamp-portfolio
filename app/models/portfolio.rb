e97119fcb9a73cc620c6c44cc1b42a3edf5b45ff
santrasatrajit <santrasatrajit@gmail.com>
2017-12-04 20:05:55 -0500

    integrated technologies model

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,9 +1,10 @@
 class Portfolio < ApplicationRecord
+  has_many :technologies
   include Placeholder
   validates_presence_of :title, :body, :main_img, :thumb_img
 
   def self.angular
-    where(:subtitle=>"Angular")
+    where(:subtitle => "Angular")
   end
 
   scope :ruby_on_rails_portfolio_items, -> {where(:subtitle=>"Ruby on Rails")} 
